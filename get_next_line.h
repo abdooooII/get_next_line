66ee718aed10baee1ee521715286a5e3670727fb
Abderrahmane Ouafsou <abouafso@e3r7p11.1337.ma>
2023-12-24 05:46:45 +0100

    update the header file

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: abouafso <abouafso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 01:26:13 by abouafso          #+#    #+#             */
-/*   Updated: 2023/12/23 05:21:44 by abouafso         ###   ########.fr       */
+/*   Updated: 2023/12/24 05:46:06 by abouafso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,6 @@ void	*ft_calloc(size_t num_elements, size_t element_size);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_join_and_free(char *buffer, char *buf);
 char	*read_file(int fd, char *result);
+char	*copy_line(char *buffer);
 
 #endif
